80d2ffae8637d193676c253c9874af91ee54419b
Kirynn <kieran1200@gmail.com>
2020-11-04 16:48:16 -0500

    Added the datetime picker

---
+++ qa327/static/js/display_elements.js
@@ -0,0 +1,8 @@
+const dtp = "#sumbit-ticket-modal > div > div > div.modal-body > form > div:nth-child(4) > div"
+
+$(function() {
+
+    $('#datetimepicker5').datetimepicker({
+        viewMode: 'years',
+    });
+});
