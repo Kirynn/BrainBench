c5ea0ccc798badd97bd6b52835d67d14d2d204ae
Kirynn <kieran1200@gmail.com>
2020-11-04 19:42:32 -0500

    Added Update Ticket Modal

--- qa327/static/js/display_elements.js
+++ qa327/static/js/display_elements.js
@@ -1,28 +1,38 @@
-const dtp = "#sumbit-ticket-modal > div > div > div.modal-body > form > div:nth-child(4) > div"
+const defaultOptions = {
+    viewMode: 'days',
+    collapse: true
+}
 
-$(function() {
+function SubmitForm(formid, location) {
+    
 
-    $('#add-datetime').datetimepicker({
-        viewMode: 'years',
+    let el = $(`form#${formid}`);
+    let data = el.serialize();
+
+    if (el.attr('method') == 'POST')
+        $.post(location, data);
+}
+
+$(function () {
+
+    $.fn.datetimepicker.Constructor.Default = $.extend({}, $.fn.datetimepicker.Constructor.Default, {
+        icons: {
+            time: 'far fa-clock',
+            date: 'far fa-calendar',
+            up: 'fa fa-arrow-up',
+            down: 'fa fa-arrow-down',
+            previous: 'fa fa-chevron-left',
+            next: 'fa fa-chevron-right',
+            today: 'fa fa-calendar-check-o',
+            clear: 'fa fa-trash',
+            close: 'fa fa-times'
+        }
     });
 
-    $('#submit_new_ticket').click(function() {
+    $('#add-datetime').datetimepicker(defaultOptions);
+    $('#update-datetime').datetimepicker(defaultOptions);
 
-
-        var data = $("form#submit_new_tickets").serialize();
-        console.log(data);
-        $.post('/login', data)
-
-        /*
-    console.log('getting all inputs....');
-
-    stuff = {
-        name
-    }
-    let outputs = [];
-    $("form#submit_new_tickets :input").each(function(){
-        outputs.push($(this).val());
-       });
-       */
-    });
+    // USING /login as api endpoint for testing, make sure to change this!!!
+    $('#submit_new_ticket').click(function () {SubmitForm('submit_new_tickets', '/login')});
+    $("#submit_update").click(function() {SubmitForm('update_ticket', '/login')});
 });
