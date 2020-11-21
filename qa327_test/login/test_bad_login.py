42bca04a28dbf80f989569220ac88b9d46bd69e6
EduardVar <eduardvarshavsky@gmail.com>
2020-11-20 19:17:00 -0500

    Initial test cases for loggining in and register
    Only half of the required register tests

---
+++ qa327_test/login/test_bad_login.py
@@ -0,0 +1,17 @@
+import pytest
+import requests
+from seleniumbase import BaseCase
+
+from qa327_test.conftest import base_url
+
+# R2: Eduard's test cases
+@pytest.mark.usefixtures('server')
+class BadLoginTest(BaseCase):
+    def test_bad_login(self):
+        """ Checks if the login failed, if correct error message appears """
+        self.open(base_url + '/login')
+        self.type("#email", "badytest@test.com")
+        self.type("#password", "FAILEDpassword!")
+        self.click('input[type="submit"]')
+        self.assert_element("#login_msg")
+        self.assert_text("login failed", "#login_msg")
