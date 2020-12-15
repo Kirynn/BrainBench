20937a05511b59763d44cf5f682b915b26190500
EduardVar <eduardvarshavsky@gmail.com>
2020-12-14 20:09:23 -0500

    Changed to update, from selling
    * Creates a seperate function for creating the ticket
    * Used in creating a ticket to be updated EVERYTIME
    * Changes EVERYTHING to update

--- qa327_test/main/test_R5.py
+++ qa327_test/main/test_R5.py
@@ -27,8 +27,18 @@ class CreatingTickets(BaseCase):
         self.type("#password2", "PYTESTpassword!")
         self.click('input[type="submit"]')
 
+    def create_ticket(self):
+        """ All fields of ticket are valid. Creates a ticket """
+        self.click("#btn-add-ticket")
+        time.sleep(1)
+        self.type("#sell-ticket-name", "testingName")
+        self.type("#sell-ticket-quantity", "10")
+        self.type("#sell-ticket-price", "20")
+        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#sell-ticket-button')
+
     # Actual testing begins
-    def test_a1_login(self):
+    def test_check_login(self):
 
         """ This test checks standard login for the Swag Labs store. """        
         self.open(base_url + '/logout')
@@ -39,169 +49,159 @@ class CreatingTickets(BaseCase):
         self.assert_element("#welcome-header")
         self.assert_text("Welcome pytest", "#welcome-header")
 
-    # All fields of ticket are valid
-    def test_all_pos(self):
-
+    def test_name_alpha_neg(self): # Test case R5.1
+        """ /update[POST] The name of the ticket is alphanumeric """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
-        
-        self.assert_element("#testingName")
+        self.create_ticket()
 
-    # Field for name of ticket isn't alphanumeric
-    def test_name_alpha_neg(self):
-
-        self.open(base_url + '/logout')
-        self.register()
-        self.login()
-        self.open(base_url)
-
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "b@d!_nam3")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "b@d!_nam3")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
 
         self.assert_element("#error_msg")
         self.assert_text("Name must be alphanumeric", "#error_msg")
 
-    # Field for name of ticket is less than 6 characters
-    def test_name_short(self):
-
+    def test_name_short(self): # Test case R5.2.0
+        """ /update[POST] The name of the ticket is no longer than 60 characters (more than 0) """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "small")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "small")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
 
         self.assert_element("#error_msg")
         self.assert_text("Name length must be between 6 and 60 characters", "#error_msg")
 
-    # Field for name of ticket is more than 60 characters
-    def test_name_long(self):
-
+    def test_name_long(self): # Test case R5.2.1
+        """ /update[POST] The name of the ticket is no longer than 60 characters (less than 61) """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        time.sleep(1)
-        self.type("#sell-ticket-name", "verylongstringohmythisisanextemelywrongnameIwonderifievenspeltextremelycorrectly")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "verylongstringohmythisisanextemelywrongnameIwonderifievenspeltextremelycorrectly")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
 
         self.assert_element("#error_msg")
         self.assert_text("Name length must be between 6 and 60 characters", "#error_msg")
 
-    # Field for quanity of ticket is less than 1
-    def test_quantity_small(self):
-
+    def test_quantity_small(self): # Test case R5.3.1
+        """ /update[POST] The quantity of the tickets has to be more than 0 """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "0")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "testingName")
+        self.type("#update-ticket-quantity", "0")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
         
         self.assert_element("#error_msg")
         self.assert_text("Please select 1 to 100 tickets", "#error_msg")
         
-    # Field for quantity of ticket is more than 100
-    def test_quantity_large(self):
-
+    def test_quantity_large(self): # Test case R5.3.2
+        """  /update[POST] The quantity of the tickets has to be less than or equal to 100 """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "1000")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "testingName")
+        self.type("#update-ticket-quantity", "1000")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
         
         self.assert_element("#error_msg")
         self.assert_text("Please select 1 to 100 tickets", "#error_msg")
 
-    # Field for price of each ticket is less than 10
-    def test_price_small(self):
-        
+    def test_price_small(self): # Test case R5.4.1
+        """ /update[POST] Price has to be more than/equal to 10 """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "5")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
-        
-        self.assert_element("#error_msg")
-        self.assert_text("Please enter an amount between 10 and 100", "#error_msg")
+        self.create_ticket()
 
-    # Field for price of each ticket is more than 100
-    def test_price_large(self):
-
-        self.open(base_url + '/logout')
-        self.register()
-        self.login()
-        self.open(base_url)
-
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "9000")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "testingName")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "5")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
         
         self.assert_element("#error_msg")
         self.assert_text("Please enter an amount between 10 and 100", "#error_msg")
 
-    # Field for date of ticket expiry is earlier than the current date
-    def test_bad_date(self):
-
+    def test_price_large(self): # Test case R5.4.2 
+        """ /update[POST] Price has to be less/than equal to 100 """
         self.open(base_url + '/logout')
         self.register()
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", "1977/12/10")
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "testingName")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "9000")
+        self.type("#update-datetime", date.today().strftime("%Y/%m/%d"))
+        self.click('#update-ticket-button')
+        
+        self.assert_element("#error_msg")
+        self.assert_text("Please enter an amount between 10 and 100", "#error_msg")
+
+    def test_bad_date(self): # Test case R5.5.1
+        """ /update[POST] Date must be given in the format YYYYMMDD (e.g. 20200901) """
+        self.open(base_url + '/logout')
+        self.register()
+        self.login()
+        self.open(base_url)
+
+        self.create_ticket()
+
+        self.click("#btn-update-testingName")
+        self.type("#update-ticket-name", "testingName")
+        self.type("#update-ticket-quantity", "10")
+        self.type("#update-ticket-price", "20")
+        self.type("#update-datetime", "1977/12/10")
+        self.click('#update-ticket-button')
         
         self.assert_element("#error_msg")
         self.assert_text("This ticket has expired", "#error_msg")
 
-    # Makes two of the exact same tickets to see how duplicates are handled by the app
+    # Seperate function to test
     def test_handles_duplicates(self):
 
         self.open(base_url + '/logout')
@@ -209,20 +209,8 @@ class CreatingTickets(BaseCase):
         self.login()
         self.open(base_url)
 
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
-
-        # Do it again for a duplicate ticket
-        self.click("#btn-add-ticket")
-        self.type("#sell-ticket-name", "testingName")
-        self.type("#sell-ticket-quantity", "10")
-        self.type("#sell-ticket-price", "20")
-        self.type("#sell-datetime", date.today().strftime("%Y/%m/%d"))
-        self.click('#sell-ticket-button')
+        self.create_ticket()
+        self.create_ticket()
 
         self.assert_element("#error_msg")
         self.assert_text("There is a ticket already specified", "#error_msg")
